04ba8cb5c3253178f8de648e4e1febe6a1cbbb00
Prince <609103450@qq.com>
2019-07-25 18:42:59 +0800

    user-js(1)

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,18 +2,21 @@
   <div id="user">
     <div class="userBox">
       <form>
+        <div class="warnning">
+            <span v-show="showWarnning" v-model="warnning">{{warnning}}</span>
+        </div>
         <div class="userImg"></div>
         <div class="userdes">
           <div class="username">
             <label for>User Name:</label>
-            <input type="text" />
+            <input type="text"  v-model="users.userName"/>
           </div>
           <div class="pas">
             <label for>Passsword:</label>
-            <input type="password" />
+            <input type="password" v-model="users.password" />
           </div>
-          <button class="sign">SIGN IN</button>
-          <button class="logon">LOGON</button>
+          <button class="sign" v-model="maxBtn" @click="oSubmit">{{maxBtn}}</button>
+          <button class="logon" v-model="minBtn" @click="logon">{{minBtn}}</button>
         </div>
       </form>
     </div>
@@ -24,10 +27,68 @@
 export default {
   name: "user",
   data() {
-    return {};
+    return {
+        ifLogon: false,
+        maxBtn: "SIGN IN",
+        minBtn: "LOGON",
+        btnName:"",
+        users:{
+            userName: "",
+            password: "",
+        },
+        warnning: "",
+        showWarnning: false
+    };
+  },
+  methods: {
+      logon(e) {
+          e.preventDefault();
+          this.btnName = this.maxBtn;
+          this.maxBtn = this.minBtn;
+          this.minBtn = this.btnName;
+          this.ifLogon = !(this.ifLogon);
+          console.log(this.ifLogon)
+      },
+      oSubmit(e) {
+          e.preventDefault();
+          if(this.ifLogon) {
+              this.$http.post("https://user-34638.firebaseio.com/post.json",this.users)
+                  .then(function (data) {
+                    console.log('用户注册成功')
+                    console.log(data)
+                  })
+          }else{
+              this.$http.get("https://user-34638.firebaseio.com/post.json")
+                  .then(function (data) {
+                      console.log(data.body);
+                      return data.body
+                  }).then(function(data) {
+                      for(let key in data){
+                          if(data[key].userName == this.users.userName){
+                              console.log("用户名存在")
+                            this.showWarnning = false;
+                              if(data[key].password == this.users.password){
+                                  console.log("密码正确")
+                                  window.location.href = "/share"
+                              }else{
+                                  console.log("密码错误")
+                                  this.warnning = "密码错误"
+                                  this.showWarnning = true;
+                              }
+                            return
+                          }
+                      }
+                      console.log("未注册");
+                      this.warnning = "未注册"
+                      this.showWarnning = true;
+                  })
+          }
+      }
   }
 };
 </script>
+
+
 <style scoped>
 #user * {
   box-sizing: border-box;
@@ -47,7 +108,21 @@ export default {
   background-color: #201f1f;
   border-radius: 5px;
   text-align: center;
-  padding:50px;
+  padding:40px;
+}
+#user .userBox .warnning{
+    height: 40px;
+    margin-bottom: 30px;
+}
+#user .userBox .warnning span{
+    display: inline-block;
+    border:1px solid crimson;
+    color:crimson;
+    width:100%;
+    height: 100%;
+    line-height:40px;
+    font-weight: bolder;
+    font-size: 19px;
 }
 #user .userBox .userImg {
   width: 50px;
@@ -80,11 +155,11 @@ export default {
     font-size: 19px;
 }
 #user .userBox .userdes .username{
-    margin:40px 0;
+    margin:30px 0;
 }
 #user .userBox button.sign{
     display: block;
-    margin:40px auto;
+    margin:30px auto;
     width:100px;
     height:50px;
     border-radius: 5px;
@@ -92,6 +167,7 @@ export default {
     background-color: #a6a2a2;
     font-size: 19px;
     cursor: pointer;
+    outline: none;
 }
 #user .userBox button.logon{
     position: absolute;
@@ -100,6 +176,7 @@ export default {
     background:none;
     border:none;
     color:#a6a2a2;
+    outline: none;
 }
 #user .userBox button.logon:hover{
     text-decoration: underline;
