a2f39b88264b9249d76a17b3a02d635346c4e6d7
Prince <609103450@qq.com>
2019-07-25 16:45:26 +0800

    user-style

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <user/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import user from './components/User'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    user
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing:border-box;
+    }
+    html,body{
+        background-color:#3c3c3c;
+    }
+    #app{
+      width:100%;
+      height: 100%;
+      min-width: 920px;
+    }
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="user">
+    <div class="userBox">
+      <form>
+        <div class="userImg"></div>
+        <div class="userdes">
+          <div class="username">
+            <label for>User Name:</label>
+            <input type="text" />
+          </div>
+          <div class="pas">
+            <label for>Passsword:</label>
+            <input type="password" />
+          </div>
+          <button class="sign">SIGN IN</button>
+          <button class="logon">LOGON</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "user",
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+#user * {
+  box-sizing: border-box;
+}
+#user {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+#user .userBox {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 60%;
+  height: 500px;
+  background-color: #201f1f;
+  border-radius: 5px;
+  text-align: center;
+  padding:50px;
+}
+#user .userBox .userImg {
+  width: 50px;
+  padding: 50px;
+  border: 1px solid #fff;
+  border-radius: 50%;
+  background: url("http://pic.yupoo.com/prince450/bbe84274/b23759fb.jpg");
+  background-size: 100%;
+  margin: 0 auto;
+}
+#user .userBox .userdes {
+}
+#user .userBox .userdes label{
+    display: inline-block;
+    width:170px;
+    /* text-align:right; */
+    padding-right:5px;
+    color:#a6a2a2;
+    font-size:25px;
+    font-weight: bolder;
+}
+#user .userBox .userdes label+input{
+    width: 300px;
+    height:30px;
+    background-color:#a6a2a2;
+    border:none;
+    border-radius:5px;
+    outline:none;
+    text-indent:1rem;
+    font-size: 19px;
+}
+#user .userBox .userdes .username{
+    margin:40px 0;
+}
+#user .userBox button.sign{
+    display: block;
+    margin:40px auto;
+    width:100px;
+    height:50px;
+    border-radius: 5px;
+    border:2px solid #201f1f;
+    background-color: #a6a2a2;
+    font-size: 19px;
+    cursor: pointer;
+}
+#user .userBox button.logon{
+    position: absolute;
+    bottom:30px;
+    right:40px;
+    background:none;
+    border:none;
+    color:#a6a2a2;
+}
+#user .userBox button.logon:hover{
+    text-decoration: underline;
+    cursor: pointer;
+}
+</style>
+
